6c6adb119bddac52b7dd3ecd72e16cbbf7be76ee
Hasib Al Rashid <69360193+Cadet-Hasib@users.noreply.github.com>
2021-01-19 21:46:08 +0600

    init

--- bot.js
+++ bot.js
@@ -33,77 +33,63 @@ bot.on("message", async (message) => {
     const command = args.shift().toLowerCase();
 
     const voiceChannel = message.member.voice.channel;
+    if (command === "start") {
+        if (!voiceChannel) {
+            return message.channel.send("You need to be in a voice channel!");
+        }
 
-    if (message.member.hasPermission("MANAGE_CHANNELS")) {
-        if (command === "start") {
-            if (!voiceChannel) {
-                return message.channel.send(
-                    "You need to be in a voice channel!"
-                );
-            }
+        const permissions = voiceChannel.permissionsFor(message.client.user);
 
-            const permissions = voiceChannel.permissionsFor(
-                message.client.user
+        if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
+            return message.channel.send(
+                "I don't have permissions to join and speak in that voice channel!"
             );
-
-            if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
-                return message.channel.send(
-                    "I don't have permissions to join and speak in that voice channel!"
-                );
-            }
-
-            voiceChannel.join().then((connection) => {
-                connectionDispatcher = connection.play(
-                    "https://coderadio-admin.freecodecamp.org/radio/8010/radio.mp3"
-                );
-                return message.channel.send("Playing!");
-            });
         }
 
-        if (command === "stop") {
-            if (connectionDispatcher) {
-                connectionDispatcher.end();
-                voiceChannel.leave();
-                return message.channel.send("Stopped");
-            }
-        }
+        voiceChannel.join().then((connection) => {
+            connectionDispatcher = connection.play(
+                "https://coderadio-admin.freecodecamp.org/radio/8010/radio.mp3"
+            );
+            return message.channel.send("Playing!");
+        });
+    }
 
-        if (command === "pause") {
-            if (connectionDispatcher) {
-                if (connectionDispatcher) {
-                    connectionDispatcher.end();
-                    return message.channel.send("Paused");
-                }
-            }
-        }
-
-        if (command === "test") {
-            message.channel.send("Tesiting Succesful");
-        }
-
-        if (command === "help") {
-            const helpEmbed = new Discord.MessageEmbed()
-                .setTitle(
-                    `CodeWave's commands list | prefix \`${config.prefix}\``
-                )
-                .addField("```!radio start```", "Starting Playing the Radio!")
-                .addField(
-                    "```!radio stop```",
-                    "Stopping the music and leaving the VC!"
-                )
-                .addField(
-                    "```!radio pause```",
-                    "Give it a Break! Just kiddin', Pause and Chill all you want without leaving the Voice Channel!"
-                )
-                .setImage(bot.user.avatarURL)
-                .setColor("RANDOM");
-            message.channel.send(helpEmbed);
+    if (command === "stop") {
+        if (connectionDispatcher) {
+            connectionDispatcher.end();
+            voiceChannel.leave();
+            return message.channel.send("Stopped");
         }
     }
-    if (!message.member.hasPermission("MANAGE_CHANNELS")) {
-        message.channel.send(
-            ":no_entry: Insufficient Permissions! Only an Admin can use this!"
-        );
+
+    if (command === "pause") {
+        if (connectionDispatcher) {
+            if (connectionDispatcher) {
+                connectionDispatcher.end();
+                return message.channel.send("Paused");
+            }
+        }
+    }
+
+    if (command === "test") {
+        message.channel.send("Tesiting Succesful");
+    }
+
+    if (command === "help") {
+        const helpEmbed = new Discord.MessageEmbed()
+            .setTitle(`CodeWave's commands list | prefix \`${config.prefix}\``)
+            .addField("```!radio start```", "Starting Playing the Radio!")
+            .addField(
+                "```!radio stop```",
+                "Stopping the music and leaving the VC!"
+            )
+            .addField(
+                "```!radio pause```",
+                "Give it a Break! Just kiddin', Pause and Chill all you want without leaving the Voice Channel!"
+            )
+            .setImage(bot.user.avatarURL)
+            .setColor("RANDOM");
+        message.channel.send(helpEmbed);
     }
 
     if (message.author.bot) return;
--- music.js
+++ music.js
@@ -1,143 +1,37 @@
-const Discord = require("discord.js");
-const { prefix } = require("./config.json");
-const ytdl = require("ytdl-core");
+const Discord = require("discord.js"),
+    client = new Discord.Client(),
+    settings = {
+        prefix: "!",
+        token: "NzkzODc1Mjc2OTk5NDI2MDQ4.X-yn9w.hzTdeLeEyvux4xsMwOxrUiq_Op8",
+    };
 
-const token = "NzkzODc1Mjc2OTk5NDI2MDQ4.X-yn9w.hzTdeLeEyvux4xsMwOxrUiq_Op8";
+const { Player } = require("discord-player");
 
-const client = new Discord.Client();
+const player = new Player(client);
+client.player = player;
 
-const queue = new Map();
+client.player.on("trackStart", (message, track) =>
+    message.channel.send(`Now playing ${track.title}...`)
+);
 
 client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-    client.user.setActivity(
-        `${client.channels.cache.size} Channels | !radio help`,
-        {
-            type: "WATCHING",
-        }
-    );
-});
-
-client.once("reconnecting", () => {
-    console.log("Reconnecting!");
-});
-
-client.once("disconnect", () => {
-    console.log("Disconnect!");
+    console.log("I'm ready !");
 });
 
 client.on("message", async (message) => {
-    if (message.author.bot) return;
-    if (!message.content.startsWith(prefix)) return;
+    const args = message.content
+        .slice(settings.prefix.length)
+        .trim()
+        .split(/ +/g);
+    const command = args.shift().toLowerCase();
 
-    const serverQueue = queue.get(message.guild.id);
+    // !play Despacito
+    // will play "Despacito" in the member voice channel
 
-    if (message.content.startsWith(`${prefix}play`)) {
-        execute(message, serverQueue);
-        return;
-    } else if (message.content.startsWith(`${prefix}skip`)) {
-        skip(message, serverQueue);
-        return;
-    } else if (message.content.startsWith(`${prefix}stop`)) {
-        stop(message, serverQueue);
-        return;
-    } else {
-        message.channel.send("You need to enter a valid command!");
+    if (command === "play") {
+        client.player.play(message, args[0]);
+        // as we registered the event above, no need to send a success message here
     }
 });
 
-async function execute(message, serverQueue) {
-    const args = message.content.split(" ");
-
-    const voiceChannel = message.member.voice.channel;
-    if (!voiceChannel)
-        return message.channel.send(
-            "You need to be in a voice channel to play music!"
-        );
-    const permissions = voiceChannel.permissionsFor(message.client.user);
-    if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
-        return message.channel.send(
-            "I need the permissions to join and speak in your voice channel!"
-        );
-    }
-
-    const songInfo = await ytdl.getInfo(args[1]);
-    const song = {
-        title: songInfo.videoDetails.title,
-        url: songInfo.videoDetails.video_url,
-    };
-
-    if (!serverQueue) {
-        const queueContruct = {
-            textChannel: message.channel,
-            voiceChannel: voiceChannel,
-            connection: null,
-            songs: [],
-            volume: 5,
-            playing: true,
-        };
-
-        queue.set(message.guild.id, queueContruct);
-
-        queueContruct.songs.push(song);
-
-        try {
-            var connection = await voiceChannel.join();
-            queueContruct.connection = connection;
-            play(message.guild, queueContruct.songs[0]);
-        } catch (err) {
-            console.log(err);
-            queue.delete(message.guild.id);
-            return message.channel.send(err);
-        }
-    } else {
-        serverQueue.songs.push(song);
-        return message.channel.send(
-            `${song.title} has been added to the queue!`
-        );
-    }
-}
-
-function skip(message, serverQueue) {
-    if (!message.member.voice.channel)
-        return message.channel.send(
-            "You have to be in a voice channel to stop the music!"
-        );
-    if (!serverQueue)
-        return message.channel.send("There is no song that I could skip!");
-    serverQueue.connection.dispatcher.end();
-}
-
-function stop(message, serverQueue) {
-    if (!message.member.voice.channel)
-        return message.channel.send(
-            "You have to be in a voice channel to stop the music!"
-        );
-
-    if (!serverQueue)
-        return message.channel.send("There is no song that I could stop!");
-
-    serverQueue.songs = [];
-    serverQueue.connection.dispatcher.end();
-}
-
-function play(guild, song) {
-    const serverQueue = queue.get(guild.id);
-    if (!song) {
-        serverQueue.voiceChannel.leave();
-        queue.delete(guild.id);
-        return;
-    }
-
-    const dispatcher = serverQueue.connection
-        .play(ytdl(song.url))
-        .on("finish", () => {
-            serverQueue.songs.shift();
-            play(guild, serverQueue.songs[0]);
-        })
-        .on("error", (error) => console.error(error));
-    dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
-    serverQueue.textChannel.send(`Start playing: **${song.title}**`);
-}
-
-client.login(token);
+client.login(settings.token);
